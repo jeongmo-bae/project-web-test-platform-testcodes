e6c9b656b5edece6268e0b75a5a8cf06f11d6f52
배정모 <jungmo@baejeongmoui-Macmini.local>
2026-01-19 13:49:37 +0900

    20260119

---
+++ src/main/java/testauto/testcode/unit/TirdTestForClassPath.java
@@ -0,0 +1,12 @@
+package testauto.testcode.unit;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TirdTestForClassPath {
+    @Test
+    @DisplayName("20260119 클래스패스를 위한 테스트 추가")
+    public void test1(){
+        System.out.println("ㅁㄴㅇㅁㄴㅇㄴㅁㅇㄴㅁㅇㅁㄴ");
+    }
+}
